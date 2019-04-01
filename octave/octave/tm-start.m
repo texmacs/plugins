24804d3a1d2d86134b354be12df2a438be9781ff
chen-chao <wenbushi@gmail.com>
2019-04-01 22:12:05 +0800

    add texmacs .m files to octave's load path

--- octave/octave/tm-start.m
+++ octave/octave/tm-start.m
@@ -1,4 +1,13 @@
 d=getenv("TEXMACS_PATH");
+
 if (length(d) > 0)
+    %% add texmacs .m files to octave's load path
+    if d(length(d)) == filesep()
+	sep = "";
+    else
+	sep = filesep();
+    endif
+    addpath([d sep "plugins/octave/octave/tm"]);
+
     tmrepl
 endif
